a7da8223ed0266839d1f2dd3d27a4e6d71e34cb4
emo <e_karov@yahoo.com>
2023-02-01 19:51:49 +0100

    Add exercise 4 implementation

--- basic/exercise_2.js
+++ basic/exercise_2.js
@@ -1,3 +1,4 @@
+// IIFE. With the imediatelly invoced function expressions we can call the function by the function declaration:
 (function greet(){
     console.log("Hi my name is Martin");
 })();
---
+++ basic/exercise_4.js
@@ -0,0 +1,7 @@
+let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+function printPlayer(arr) {
+    console.log(`The most valuable player of the match is ${arr[2]}`);
+}
+
+printPlayer(players)
