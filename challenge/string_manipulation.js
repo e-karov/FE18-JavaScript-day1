e4f2bc7798e7b35a49d988d4dfbde80ab78d9f41
emo <e_karov@yahoo.com>
2023-02-01 22:17:18 +0100

    Add comments in 'string manipulation' for the used methods

--- challenge/string_manipulation.js
+++ challenge/string_manipulation.js
@@ -1,5 +1,5 @@
 let str = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
 
-let result = str.split("$").join(" ");
+let result = str.split("$").join(" "); // Using the String.split() method we get an array from the string splitted by the character given as argument. Then by joining this array with an empty space character, we get the expected result.
 
 console.log(result);
